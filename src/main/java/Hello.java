69e11e3be44fdeb442d8d5d91c7d021a538aaa71
ramizkazak <ramizkazak@gmail.com>
2021-01-26 14:38:01 -0600

    movie2 branch watching on the phone for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World B22");
         System.out.println("B21 future SDET learning Git");
+        //movie on the phone 10 minutes
 
     }
 }
